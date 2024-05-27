f697870cef6007ce5b453954b785403d328d0f06
fazil-syed <syedfazil539@gmail.com>
2024-05-27 18:07:09 +0530

    create get user listings api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,10 +1,11 @@
-import Listing from "../models/listing.model.js"
+import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const createListing = async (req,res,next)=>{
-    try {
-        const listing = await Listing.create(req.body);
-        res.status(201).json(listing)
-    } catch (error) {
-        next(error)
-    }
-}
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,15 @@
-
-import express from 'express';
-import { deleteUser, updateUserInfo } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import {
+  deleteUser,
+  getUserListing,
+  updateUserInfo,
+} from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
+router.post("/update/:id", verifyToken, updateUserInfo);
+router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListing);
 
-router.post('/update/:id',verifyToken,updateUserInfo)
-router.delete('/delete/:id',verifyToken,deleteUser)
-
-export default router
+export default router;
