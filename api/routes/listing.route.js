02b36d3b16224acf36e93c5e54e4c56caea2e0de
fazil-syed <syedfazil539@gmail.com>
2024-05-31 19:57:39 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,12 +2,14 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
+router.post("/update/:id", verifyToken, updateListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 
 export default router;
