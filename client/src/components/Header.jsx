2fae86da6b4240562fd3af4a14c53c2648e0aaf7
fazil-syed <syedfazil539@gmail.com>
2024-06-04 18:42:49 +0530

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,41 +1,79 @@
-import React from 'react'
-import {FaSearch} from "react-icons/fa"
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 const Header = () => {
-  const {currentUser} = useSelector((state)=>state.user)
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const urlSearchTerm = urlParams.get("searchTerm");
+    if (urlSearchTerm) {
+      setSearchTerm(urlSearchTerm);
+    }
+  }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
-        <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
-        <Link to={'/'}>
-        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Syed</span>
-            <span className='text-slate-700'>Estate</span>
-        </h1>
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
+        <Link to={"/"}>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Syed</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-slate-600'/>
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
-        <ul className='flex gap-4'>
-            <Link to={'/'}>
-            <li className='hidden sm:inline text-slate-700 hover:underline hover:cursor-pointer'>Home</li>
+        <ul className="flex gap-4">
+          <Link to={"/"}>
+            <li className="hidden sm:inline text-slate-700 hover:underline hover:cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to={"/about"}>
+            <li className="hidden sm:inline text-slate-700 hover:underline hover:cursor-pointer">
+              About
+            </li>
+          </Link>
+          {currentUser ? (
+            <Link to={"/profile"}>
+              <li>
+                <img
+                  src={currentUser.avatar}
+                  alt={currentUser.username}
+                  className="rounded-full h-7 w-7 object-cover"
+                />
+              </li>
             </Link>
-            <Link to={'/about'}>
-            <li className='hidden sm:inline text-slate-700 hover:underline hover:cursor-pointer'>About</li>
+          ) : (
+            <Link to={"/sign-in"}>
+              <li className="hover:cursor-pointer text-slate-700">Sign in</li>
             </Link>
-            {
-              currentUser ? (<Link to={'/profile'}>
-              <li><img src={currentUser.avatar} alt={currentUser.username} className='rounded-full h-7 w-7 object-cover'/></li>
-              </Link>):(<Link to={'/sign-in'}>
-              <li className='hover:cursor-pointer text-slate-700'>Sign in</li>
-              </Link>)
-            }
-            
+          )}
         </ul>
-        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
