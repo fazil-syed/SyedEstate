cdcd31f17ef15233009341721cd661f22ee3f37a
fazil-syed <syedfazil539@gmail.com>
2024-06-08 00:05:27 +0530

    add pre deployment configurations for render

--- api/index.js
+++ api/index.js
@@ -1,37 +1,43 @@
 import { configDotenv } from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import userRouter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
-import listingRouter from "./routes/listing.route.js"
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-
-configDotenv()
+import path from "path";
+configDotenv();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO).then(()=>console.log("Connected to DB")).catch((e)=>console.log(e))
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to DB"))
+  .catch((e) => console.log(e));
 
+const __dirname = path.resolve();
 
-app.use("/api/user",userRouter)
+app.use("/api/user", userRouter);
 
-app.use('/api/auth',authRouter)
-app.use('/api/listing',listingRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-app.use((err,req,res,next)=>{
-    const statuscode = err.statusCode || 500
-    const message = err.message || "Internal Server Error"
-    return res.status(statuscode).json({
-        success: false,
-        statuscode,
-        message
-    })
-})
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+app.use((err, req, res, next) => {
+  const statuscode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statuscode).json({
+    success: false,
+    statuscode,
+    message,
+  });
+});
 
-
-
-app.listen(3000,()=>console.log("Server running at port 3000!!!"))
+app.listen(3000, () => console.log("Server running at port 3000!!!"));
